b81c5e14321b92fc650c51eaaab6f79217f9390e
Sagar Dhakal <sagardh@gmail.com>
2011-07-19 20:11:58 -0400

    hellos

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ class TestExample(unittest.TestCase):
     def test_always_true(self):
         self.assertEqual(1, 1)
 
-if __name__ == '__main__':
+if s__name__ == '__main__':
     unittest.main()
 
 
